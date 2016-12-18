3d5092e64d8b8ecb47967efada690da29f0aa9aa
Benjamin <bpe.krafft@gmail.com>
2016-12-18 20:31:43 +0100

    Update GUI

--- app/src/main/java/com/example/benjamin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/benjamin/myapplication/MainActivity.java
@@ -1,21 +1,11 @@
 package com.example.benjamin.myapplication;
 
-import android.app.DatePickerDialog;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private DatePickerDialog dpd= null;
-    private DatePickerDialog.OnDateSetListener odsl;
-    private static final int NOTIF_ID=0;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
